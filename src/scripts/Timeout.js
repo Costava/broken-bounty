bdd9979ee9829cc61511f4f4077801caf3ae129b
Costava <Costava@users.noreply.github.com>
2016-09-13 03:25:44 -0500

    Add Timeout.js

---
+++ src/scripts/Timeout.js
@@ -0,0 +1,31 @@
+function Timeout(work, delay) {
+	this.work = work;
+	this.delay = delay;
+
+	this.waiting = false;
+	this.timeout;
+}
+
+Timeout.prototype.set = function() {
+	if (this.waiting) {
+		clearTimeout(this.timeout);
+	}
+
+	this.timeout = setTimeout(function() {
+		this.waiting = false;
+
+		this.work();
+	}.bind(this), this.delay);
+
+	this.waiting = true;
+};
+
+Timeout.prototype.clear = function() {
+	if (this.waiting) {
+		clearTimeout(this.timeout);
+
+		this.waiting = false;
+	}
+};
+
+export default Timeout
